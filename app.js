07db168ed2fb9f97307d2a1b246931a93b6dff7a
Hernan Hlace <hernan@hmbrokers.com>
2020-05-13 11:03:32 -0300

    index renderizado

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require( 'express' );
 const port = 3000;
 const morgan = require( 'morgan' );
 const app = express();
+const nunjucks = require('nunjucks');
+
+app.set('view engine', 'html'); // hace que res.render funcione con archivos html
+app.engine('html', nunjucks.render); // cuando le den archivos html a res.render, va a usar nunjucks
+nunjucks.configure('views', {noCache: true}); // apunta a nunjucks al directorio correcto para los templates
+
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+//res.render( 'index', {title: 'Hall of Fame', people: people});
+
+app.get('/', (req, res) => res.render( 'index', {title: 'Hall of Fame', people: people}));
 
 app.use(morgan("tiny"))
 
@@ -16,4 +26,3 @@ app.get('/special', (req, res) => res.send('Hola SPECIAL!!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
-//hola soy seba
